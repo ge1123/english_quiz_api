b32960f1492232fce8a6e91666b04ec9d538e76e
ge1123 <as8354093@gmail.com>
2025-06-11 18:13:09 +0800

    feat: 登入、註冊加上驗證功能

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class LoginRequest(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=1, description="使用者名稱不得為空")
+    password: str = Field(..., min_length=1, description="密碼不得為空")
+
 
 class RegisterRequest(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=4, description="使用者名稱不得為空")
+    password: str = Field(..., min_length=4, description="密碼不得為空")
+
 
 class TokenResponse(BaseModel):
     access_token: str
