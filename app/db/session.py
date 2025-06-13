6656b58869e8c0fa114c0d26b32e6cda0cd3ee26
ge1123 <as8354093@gmail.com>
2025-06-13 17:28:53 +0800

    chore: rename db_config.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+from app.core.db_config import settings
 
 DATABASE_URL = settings.database_url
 engine = create_async_engine(DATABASE_URL, echo=False)
